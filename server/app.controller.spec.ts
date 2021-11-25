85ba438e140b4cae9af6494c8aa224c9cd04d03d
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2021-11-25 09:49:14 -0500

    Fixing CodeQL to run on typescript.

--- server/app.controller.spec.ts
+++ server/app.controller.spec.ts
@@ -7,7 +7,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [],
+      providers: []
     }).compile();
 
     appController = app.get<AppController>(AppController);
--- src/app/admin/admin/admin.component.spec.ts
+++ src/app/admin/admin/admin.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
+import { MenuComponent } from '../menu/menu.component';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -8,7 +9,7 @@ describe('AdminComponent', () => {
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [AdminComponent]
+      declarations: [AdminComponent, MenuComponent]
     }).compileComponents();
   });
 
