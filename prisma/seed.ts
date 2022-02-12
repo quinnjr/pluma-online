67efe73b579caa53b00bdd83a1fdf0b4cba2caf8
Joseph Quinn <423821+quinnjr@users.noreply.github.com>
2022-02-12 14:37:28 -0500

    Adding fake user account to database seed.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import argon2 from 'argon2';
 
-import { Plugin } from '../server/@generated/prisma-graphql/plugin/plugin.model';
-import { Pipeline } from '../server/@generated/prisma-graphql/pipeline';
+import { Role } from '../server/@generated/prisma-graphql/prisma';
 
 import { Plugins } from './plugins';
 import { Pipelines } from './pipelines';
@@ -9,7 +9,6 @@ import { Pipelines } from './pipelines';
 const prisma = new PrismaClient();
 
 async function seed() {
-
   for (const plugin of Plugins) {
     const p = await prisma.plugin.upsert({
       where: {
@@ -18,7 +17,6 @@ async function seed() {
       update: plugin,
       create: plugin
     });
-
   }
 
   console.log(`Seeded ${Plugins.length} plugins`);
@@ -34,6 +32,24 @@ async function seed() {
   }
 
   console.log(`Seeded ${Pipelines.length} pipelines`);
+
+  const password = await argon2.hash('password');
+
+  let user: any = {
+    email: 'test@localhost',
+    passwordHash: password,
+    displayName: 'Test Account',
+    role: Role.Admin,
+    enabled: true
+  };
+
+  user = await prisma.user.upsert({
+    where: {
+      email: 'test@localhost'
+    },
+    update: user,
+    create: user
+  });
 }
 
 seed()
