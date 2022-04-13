5b7614b1f5fa5df1b5e5366a056f84ded8cd544e
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2022-04-13 12:34:48 -0400

    Adding `canAccess` query to auth resolver.

--- server/auth/auth.resolver.ts
+++ server/auth/auth.resolver.ts
@@ -1,12 +1,19 @@
 import { Resolver, Args, Query, Mutation } from '@nestjs/graphql';
 import { User, UserCreateInput } from '../@generated/prisma-graphql/user';
-import { AuthService } from './auth.service';
 
+import { User as UserDecorator } from '../user.decorator';
+import { CaslAbilityFactory, Subjects } from '../casl/casl-ability.factory';
+import { Action } from '../casl/action';
+
+import { AuthService } from './auth.service';
 import { AuthResponse } from './auth-response';
 
 @Resolver()
 export class AuthResolver {
-  constructor(private readonly $authService: AuthService) {}
+  constructor(
+    private readonly $authService: AuthService,
+    private readonly $caslAbilityFactory: CaslAbilityFactory
+  ) {}
 
   @Query((returns) => AuthResponse)
   public async login(
@@ -31,5 +38,14 @@ export class AuthResolver {
     @Args('code', { type: () => String, nullable: false }) code: string
   ): Promise<boolean> {
     return this.$authService.verify(userId, code);
+  }
+
+  @Query((returns) => Boolean)
+  public canAccess(
+    @Args('claim', { type: () => Action, nullable: false }) claim: Action,
+    @UserDecorator() user: User
+  ): boolean {
+    const ability = this.$caslAbilityFactory.createForUser(user);
+    return ability.can(Action.Read, claim as unknown as Subjects);
   }
 }
