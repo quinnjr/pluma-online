02829eb159ed56f6924c8a552cac91c63154c9c9
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2022-06-05 21:42:12 -0400

    updating dependenceies. adding testing to jwt interceptor

--- src/app/jwt.interceptor.spec.ts
+++ src/app/jwt.interceptor.spec.ts
@@ -3,11 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { JwtInterceptor } from './jwt.interceptor';
 
 describe('JwtInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      JwtInterceptor
-      ]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [JwtInterceptor]
+    })
+  );
 
   it('should be created', () => {
     const interceptor: JwtInterceptor = TestBed.inject(JwtInterceptor);
